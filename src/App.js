8d4bd42c3f538519f62b153c18e9261aed28d778
Imanalopher <iman.mamadiyev@gmail.com>
2018-05-07 17:01:53 +0600

    Material-ui installed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <AppBar title="Listy">
+        </AppBar>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Index = () => (
+    <MuiThemeProvider>
+        <App />
+    </MuiThemeProvider>
+);
+ReactDOM.render(<Index />, document.getElementById('root'));
 registerServiceWorker();
